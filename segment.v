ecd8d0df9fe09ecfce8bcb089bdbd1f661513745
paulonavarro <pnavarro@itba.edu.ar>
2018-11-19 17:45:44 -0300

    Cambio de segment.v
    Lo cambie para que se pase todo en una variable

--- segment.v
+++ segment.v
@@ -1,100 +1,32 @@
-module segment(A , B , C , D , E , F , G , in );
-//  los valores A,B,C,D,E,F y G son los segmentos para mostrar un numero
+module segment( segment7 , in );
+//  segment7 es la salida que devuelve un numero binario de
 //  in es el numero que entra, se esoera que el numero sea un binario de 4 bits
 
 input in;
-output reg A , B , C , D , E , F , G;
-parameter low = 1'b0 , high = 1'b1 ;
+output reg [6:0]segment7; // A , B , C , D , E , F , G 
+
+/*parameter low = 1'b0 , high = 1'b1 ; ESTOS VALORES SON PARA HACER EL SEGMENTADO VALOR A VALOR*/
 
 always @ (in)
 begin
   case (in)
-  4'b0000: 
-  begin
-    A = high;
-    B = high;
-    C = high;
-    D = high;
-    E = high;
-    F = high;
-    G = low;
-  end
-  4'b0001: 
-  begin
-    A = low;
-    B = high;
-    C = high;
-    D = low;
-    E = low;
-    F = low;
-    G = low;    
-  end
-  4'b0010: 
-  begin
-    A = high;
-    B = high;
-    C = low;
-    D = high;
-    E = high;
-    F = low;
-    G = high;
-    
-  end
-  4'b0011: 
-  begin
-    A = high;
-    B = high;
-    C = high;
-    D = high;
-    E = low;
-    F = low;
-    G = high;    
-  end
-  4'b0100: 
-  begin
-    A = low;
-    B = high;
-    C = high;
-    D = low;
-    E = low;
-    F = high;
-    G = high;    
-  end
+  4'b0000: segment7 = 7'b1111110 ;
 
-  4'b0101: 
-  begin
-    A = high;
-    B = low;
-    C = high;
-    D = high;
-    E = low;
-    F = high;
-    G = high;    
-  end
+  4'b0001: segment7 = 7'b0110000 ; 
 
-  4'b0110: 
-  begin
-    A = high;
-    B = low;
-    C = high;
-    D = high;
-    E = high;
-    F = high;
-    G = high;    
-  end
+  4'b0010: segment7 = 7'b1101101 ; 
 
-  4'b0111: 
-  begin
-    A = high;
-    B = high;
-    C = high;
-    D = low;
-    E = low;
-    F = low;
-    G = low;
-  end
+  4'b0011: segment7 = 7'b1111001 ; 
 
-  4'b1000: 
+  4'b0100: segment7 = 7'b0110011 ; 
+
+  4'b0101: segment7 = 7'b1011011 ;
+
+  4'b0110: segment7 = 7'b1011111 ;
+
+  4'b0111: segment7 = 7'b1110000 ;
+
+  4'b1000: segment7 = 7'b1111111 ; /* EJEMPLO PARA VALORES DE SEGMENTOS VALOR A VALOR
   begin
     A = high;
     B = high;
@@ -103,29 +35,11 @@ begin
     E = high;
     F = high;
     G = high;  
-  end
+  end */
 
-  4'b1001: 
-  begin
-    A = high;
-    B = high;
-    C = high;
-    D = low;
-    E = low;
-    F = high;
-    G = high;  
-  end
+  4'b1001: segment7 = 7'b1110011 ; 
 
-  default: 
-  begin    
-    A = low;
-    B = low;
-    C = low;
-    D = low;
-    E = low;
-    F = low;
-    G = low;
-  end
+  default: segment7 = 7'b1111110 ;
 
 end
 
